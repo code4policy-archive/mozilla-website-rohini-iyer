d097156c60c4233b0a12fc4307e0ffdd37161e03
Rohini Iyer <rohini_iyer@hks.harvard.edu>
2022-01-08 12:10:51 -0500

    add show and hide button for fox

--- main.js
+++ main.js
@@ -8,3 +8,18 @@
  
  // Assign the function to the onclick event on that element
  foxImage.onclick = sayOuch;
+
+ function show() {
+ 
+ /* Access image by id and change the display property to block*/
+            document.getElementById('image')
+                    .style.display = "block";
+ 
+            document.getElementById('')
+                    .style.display = "none";
+        }
+
+function hide() { 
+            document.getElementById('image')
+            .style.visibility = 'hidden';        
+}
---
+++ rough.js
@@ -0,0 +1,23 @@
+function show() {
+ 
+ /* Access image by id and change the display property to block*/
+            document.getElementById('image')
+                    .style.display = "block";
+ 
+            document.getElementById('')
+                    .style.display = "none";
+        }
+
+
+function hide() {
+
+}
+
+type="mybutton" onclick="show()" 
+type="mybutton" onclick="hide()" 
+
+document.getElementById('image')
+                    .style.display = "block";
+ 
+            document.getElementById('')
+                    .style.display = "none";
